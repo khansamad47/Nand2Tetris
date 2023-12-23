8509cff635834639a16dfbd25efc0329fb2c8752
khansamad47 <khan.samad47@gmail.com>
2023-12-23 00:37:28 -0500

    Adding solution for week 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,39 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 //// Replace this comment with your code.
+
+
+(MAINLOOP)
+    @KBD
+    D=M;
+    @SETTOWHITE
+    D;JEQ          // If no keyboard input then set value to 0
+    @pixel
+    M=-1
+    @SETSCREEN
+    0;JMP
+    (SETTOWHITE)
+        @pixel
+        M=0
+    // END of SETTOWHITE
+    (SETSCREEN)
+        @SCREEN
+        D=A
+        @screenptr
+        M=D                    // screenptr = &screen[0]
+        (LOOP)
+            @KBD
+            D=A 
+            @screenptr
+            D=D-M
+            @MAINLOOP
+            D;JEQ              // If &KBD - screenptr == 0 then restart
+            @pixel
+            D=M
+            @screenptr
+            A=M                // *screenptr
+            M=D                // *screenptr = pixel
+            @screenptr
+            M=M+1              // screenptr++
+            @LOOP
+            0;JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -8,3 +8,27 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 //// Replace this comment with your code.
+
+@i        // Initialize i = 0
+M=0
+@R2        // Initialize i = 0
+M=0
+(LOOP)
+    @R1
+    D=M
+    @i
+    D=D-M
+    @END
+    D;JEQ   // If R1 - i == 0 then END
+    @R0
+    D=M
+    @R2
+    M=M+D   // R2 = R2 + R0
+    @i
+    M=M+1   // i++
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
